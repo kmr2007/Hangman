6ac8bf046f6bfbd085f158343594abbff571f026
kmr2007 <kaydenceriehl@gmail.com>
2023-02-28 13:16:58 -0700

    xfcb

--- multiplayer.js
+++ multiplayer.js
@@ -10,7 +10,7 @@ let word = prompt(
   "Enter one word between 4 and 11 letters. (Don't let the other player see!)"
 ).toLowerCase();
 let wordLength = word.length;
-let wordLengthEl = document.getElementById("wordLength");
+let wordLengthEl = wordLengthEl;
 let incorrectGuesses = 0;
 let gameBegin = "false";
 
@@ -43,7 +43,7 @@ function setUp() {
   } else {
     alert("Something went wrong. Refresh and try again");
   }
-  document.getElementById("wordLength").innerHTML = wordLength;
+  wordLengthEl.innerHTML = wordLength;
   document.getElementById("beginGameBtn").innerHTML = "";
   gameBegin = "true";
 }
@@ -66,34 +66,34 @@ function letterGuessing() {
     alert("Please enter a 1 letter guess.");
   } else if (numberOfOccurences === 1) {
     wordLength = wordLength.replaceAt(letterPosition, letterGuessed);
-    document.getElementById("wordLength").innerHTML = wordLength;
+    wordLengthEl.innerHTML = wordLength;
   } else if (numberOfOccurences === 2) {
     wordLength = wordLength.replaceAt(letterPosition, letterGuessed);
-    document.getElementById("wordLength").innerHTML = wordLength;
+    wordLengthEl.innerHTML = wordLength;
 
     wordLength = wordLength.replaceAt(letterPosition2, letterGuessed);
-    document.getElementById("wordLength").innerHTML = wordLength;
+    wordLengthEl.innerHTML = wordLength;
   } else if (numberOfOccurences === 3) {
     wordLength = wordLength.replaceAt(letterPosition, letterGuessed);
-    document.getElementById("wordLength").innerHTML = wordLength;
+    wordLengthEl.innerHTML = wordLength;
 
     wordLength = wordLength.replaceAt(letterPosition2, letterGuessed);
-    document.getElementById("wordLength").innerHTML = wordLength;
+    wordLengthEl.innerHTML = wordLength;
 
     wordLength = wordLength.replaceAt(letterPosition3, letterGuessed);
-    document.getElementById("wordLength").innerHTML = wordLength;
+    wordLengthEl.innerHTML = wordLength;
   } else if (numberOfOccurences === 4) {
     wordLength = wordLength.replaceAt(letterPosition, letterGuessed);
-    document.getElementById("wordLength").innerHTML = wordLength;
+    wordLengthEl.innerHTML = wordLength;
 
     wordLength = wordLength.replaceAt(letterPosition2, letterGuessed);
-    document.getElementById("wordLength").innerHTML = wordLength;
+    wordLengthEl.innerHTML = wordLength;
 
     wordLength = wordLength.replaceAt(letterPosition3, letterGuessed);
-    document.getElementById("wordLength").innerHTML = wordLength;
+    wordLengthEl.innerHTML = wordLength;
 
     wordLength = wordLength.replaceAt(letterPosition4, letterGuessed);
-    document.getElementById("wordLength").innerHTML = wordLength;
+    wordLengthEl.innerHTML = wordLength;
   } else if (numberOfOccurences === 0) {
     ++incorrectGuesses;
     document.getElementById(
@@ -109,5 +109,6 @@ function letterGuessing() {
     document.getElementById("winLose").innerHTML = "You Win!";
   } else if (incorrectGuesses === 10) {
     document.getElementById("winLose").innerHTML = "You Lose!";
+    wordLengthEl.innerHTML = `The word was ${word}!`;
   }
 }
